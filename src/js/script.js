36ab5f21b87073232977002723f2dbb6139e5451
luiz-fischer <luiz.fischer@alunos.sc.senac.br>
2021-10-16 22:11:04 -0300

    feat: ok

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+/* Carregar o menu lateral */
+$.get("navbar.html", function (data) {
+    $("#nav-placeholder").replaceWith(data);
+});
+
+/* Função Sub Menu */
+$(document).on("click", ".side-menu > ul > li", function (e) {
+    if ($(this).find(".sub-menu").length > 0) {
+      e.preventDefault();
+      e.stopPropagation();
+      $(this).toggleClass("active");
+      $(this).find(".sub-menu").stop().slideToggle();
+    }
+  });
+  
